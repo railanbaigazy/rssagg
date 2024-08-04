055857507a3dcbd0f5b5bea394579436cef4c006
railanbaigazy <baigazy.railan@gmail.com>
2024-08-04 13:47:12 +0500

    robust parse pubDate

--- scraper.go
+++ scraper.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"database/sql"
+	"fmt"
 	"log"
 	"strings"
 	"sync"
@@ -55,7 +56,7 @@ func scrapeFeed(db *database.Queries, wg *sync.WaitGroup, feed database.Feed) {
 			description.String = item.Description
 			description.Valid = true
 		}
-		publishedAt, err := time.Parse(time.RFC1123Z, item.PubDate)
+		publishedAt, err := parsePublishedAt(item.PubDate)
 		if err != nil {
 			log.Printf("Couldn't parse date %v with err %v", item.PubDate, err)
 		}
@@ -78,4 +79,30 @@ func scrapeFeed(db *database.Queries, wg *sync.WaitGroup, feed database.Feed) {
 		}
 	}
 	log.Printf("Feed %s collected, %v posts found", feed.Name, len(rssFeed.Channel.Item))
+}
+
+func parsePublishedAt(pubDate string) (time.Time, error) {
+	var publishedAt time.Time
+	var err error
+
+	dateFormats := []string{
+		time.RFC1123Z,                   // e.g., "Mon, 02 Jan 2006 15:04:05 -0700"
+		time.RFC1123,                    // e.g., "Mon, 02 Jan 2006 15:04:05 MST"
+		time.RFC822Z,                    // e.g., "02 Jan 06 15:04 -0700"
+		time.RFC822,                     // e.g., "02 Jan 06 15:04 MST"
+		time.RFC3339,                    // e.g., "2006-01-02T15:04:05Z07:00"
+		time.RFC3339Nano,                // e.g., "2006-01-02T15:04:05.999999999Z07:00"
+		"2006-01-02T15:04:05Z",          // e.g., "2006-01-02T15:04:05Z"
+		"Mon, 2 Jan 2006 15:04:05 MST",  // e.g., "Mon, 2 Jan 2006 15:04:05 MST"
+		"Mon, 02 Jan 2006 15:04:05 MST", // e.g., "Mon, 02 Jan 2006 15:04:05 MST"
+	}
+
+	for _, format := range dateFormats {
+		publishedAt, err = time.Parse(format, pubDate)
+		if err == nil {
+			return publishedAt, nil
+		}
+	}
+
+	return publishedAt, fmt.Errorf("unable to parse date: %s", pubDate)
 }
